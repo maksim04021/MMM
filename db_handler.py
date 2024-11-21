3fb3a7caf9b14957c1451e30febafb1ca0e9f7e8
maksim04021 <147934173+maksim04021@users.noreply.github.com>
2024-11-21 18:51:35 +0300

    Update db_handler.py

--- db_handler.py
+++ db_handler.py
@@ -1,6 +1,4 @@
 import sqlite3
-import time
-import random
 
 
 def login(login, password, signal):
@@ -11,16 +9,17 @@ def login(login, password, signal):
     cur.execute("SELECT password FROM user WHERE name = ?", (login.text(),))
     value = cur.fetchall()
 
-    if value != []:
+    if value:
         if value[0][0] == password.text():
             signal.emit('Успешная авторизация')
         else:
             signal.emit('Проверьте данные и повторите попытку')
     else:
         signal.emit('Логин не найден, проверьте правильность ввода или зарегистрируйтесь')
-    
+
     cur.close()
     con.close()
+
 
 def register(login, password, signal):
     con = sqlite3.connect('HER.db')
@@ -30,15 +29,12 @@ def register(login, password, signal):
     cur.execute("SELECT password FROM user WHERE name = ?", (login.text(),))
     value = cur.fetchall()
 
-    if value != []:
+    if value:
         signal.emit('Логин занят!')
-    elif value == []:
-        timestamp = int(time.time() * 1000)  # Текущая временная метка в миллисекундах
-        random_number = random.randint(1000, 9999)  # Случайное число
-        unique_id = f"{timestamp}-{random_number}"
-        cur.execute("INSERT INTO user (id, name, password) VALUES (?, ?, ?)", (unique_id, login.text(), password.text()))
+    elif not value:
+        cur.execute("INSERT INTO user (name, password) VALUES (?, ?)", (login.text(), password.text()))
         signal.emit('Вы успешно зарегистрированы!')
         con.commit()
-    
+
     cur.close()
     con.close()
