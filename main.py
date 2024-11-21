dbfd065ab43afe84b705da332ddd07b973da170a
maksim04021 <147934173+maksim04021@users.noreply.github.com>
2024-11-21 18:50:54 +0300

    main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import sys
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from check import *
-from des import *
-import uuid
+
 
 class Main(QMainWindow):
     def __init__(self):
@@ -14,30 +13,32 @@ class Main(QMainWindow):
         self.base_line_edit = [self.Login, self.Password]
         self.check = CheckThread()
         self.check.Mysignal.connect(self.signal_handler)
-    
-    #Проверка правильности ввода
+
+    # Проверка правильности ввода
     def check_in(funct):
         def wrapper(self):
             for line_edit in self.base_line_edit:
                 if len(line_edit.text()) == 0:
                     return
             funct(self)
+
         return wrapper
 
     def signal_handler(self, value):
         QtWidgets.QMessageBox.about(self, 'Оповещение', value)
-    
+
     @check_in
     def auth(self):
         name = self.Login
         passw = self.Password
         self.check.thr_login(name, passw)
-    
+
     @check_in
     def register(self):
         name = self.Login
         passw = self.Password
         self.check.thr_register(name, passw)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
