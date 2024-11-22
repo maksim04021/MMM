a4a6dbd9c414ebe01df6f1e12af0e2f2802fe916
maksim04021 <147934173+maksim04021@users.noreply.github.com>
2024-11-22 22:46:20 +0300

    Update check.py

--- check.py
+++ check.py
@@ -1,12 +1,22 @@
-from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6 import QtCore
 from db_handler import *
+import logging
 
 
 class CheckThread(QtCore.QThread):
     Mysignal = QtCore.pyqtSignal(str)
 
     def thr_login(self, name, password):
+        logging.debug(f"CheckThread.thr_login: name = {name}, password = {password}")  # логирование
+        if not name or not password:
+            self.Mysignal.emit("Ошибка: Не все поля заполнены")
+            return False
         login(name, password, self.Mysignal)
-    
+        return True
+
     def thr_register(self, name, password):
+        logging.debug(f"CheckThread.thr_register: name = {name}, password = {password}")  # логирование
+        if not name or not password:
+            self.Mysignal.emit("Ошибка: Не все поля заполнены")
+            return
         register(name, password, self.Mysignal)
